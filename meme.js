37dff9912ec87497f668d7cf8c77380f9528f571
mroatmeal <jfeind101@gmail.com>
2025-03-12 14:29:40 -0500

    completed

--- meme.js
+++ meme.js
@@ -1,14 +1,4 @@
-// document.getElementById('meme-Generator') // getElementById to get one element back
-// document.getElementById('submit') 
-// document.getElementsByTagName('submit') // getElementByTagName to get HTML collection back
-// document.getElementsByClassName('submit') // getElementByClassName to get HTML collection back
-
-// querySelector accepts a string which is valid CSS selector
-// returns the first element that matches the CSS selector passed to the function.
-// querySelectorAll gets all of the elements that match
-// let btn = document.querySelector('#submit')
-// let inputs = document.querySelectorAll("input")
-
+// Function to make the letters for the page title to change colors randomly. 
 function randomRGB(){
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
@@ -26,7 +16,6 @@ function randomRGB(){
 //     h1.style.color = randomRGB()
 // }, 300)
 
-// This is how you change color of all the letters indivi using the span and class on html
 const letters = document.querySelectorAll('.letter')
 
 setInterval(function () {
@@ -34,9 +23,6 @@ setInterval(function () {
         letter.style.color = randomRGB()
     }
 }, 900)
-
-
-// inputImg.setAttribute = ('src', 'https://en.wikipedia.org/wiki/Tabby_cat#/media/File:Cat_November_2010-1a.jpg')
 
 // switch for light and dark mode on site and will remember pref
 const toggleSwitch = document.querySelector('input[type="checkbox"]')
@@ -59,35 +45,64 @@ toggleSwitch.addEventListener('click', function(e){
     
 })
 
-
+// Function that takes the users inputs and outputs them in a div.
 function makeMeme() {
 
+    // This is making a var to select the html input with the id of memeImg
     const inputImg = document.querySelector("#memeImg").value
+    // This creates an empty img element so that you can store the users input img
     const newMemeImg = document.createElement('img')
     
+    // if the inputImg has a value ie it is true
     if(inputImg){
+        // This sets the empty img element created above to have its source value equal to the users input
         newMemeImg.src = inputImg;
-        newMemeImg.className = 'newImg'
+        // This adds the class of newImg to the newMemeImg so that you can select the class in CSS for styling
+        newMemeImg.classList = 'newImg'
     }
 
+    // making a var to select the html input with the id of #topText
     const topText = document.querySelector('#topText')
+    // creating a div element to store the inputs if there is any 
     const newMemeTopText = document.createElement('div')
+    // setting the textContent property to equal the topText value 
     newMemeTopText.textContent = topText.value
+    // This adds the class of topText to the newMemeTopText so that you can select the class in CSS for styling
     newMemeTopText.classList.add('topText')
 
+    // making a var to select the html input with the id of #bottomText
     const bottomText = document.querySelector('#bottomText')
+    // creating a div element to store the inputs if there is any 
     const newMemeBottomText = document.createElement('div')
+    // setting the textContent property to equal the bottomText value 
     newMemeBottomText.textContent = bottomText.value
+    // This adds the class of bottomText to the newMemeBottomText so that you can select the class in CSS for styling
     newMemeBottomText.classList.add('bottomText')
 
-
+    // making a var with a button element with the intention of being able to remove elements in the div called meme-container
     const removeButton = document.createElement('button')
-    removeButton.id = '#removeMe'
-    removeButton.innerText = "Remove"
+    // setting innerText property to Remove so that the button says remove on it
+    removeButton.innerText = 'Remove'
+    // adding class for easy CSS styling selection
+    removeButton.classList.add('removeBtn')
 
-    const newMeme = document.querySelector("#results")
-    newMeme.append(newMemeTopText, newMemeBottomText, newMemeImg, removeButton)
-    console.log(newMeme)
+    // adding event listerner so that the remove button when clicked with remove what is in the div called memeContainer 
+    removeButton.addEventListener('click', function(){
+        memeContainer.remove();
+    })
+
+    // creating a div element to put the captured img, topText, bottomText, and removeButton for each meme
+    // in its own container. This is so each meme can be selected individually so that the remove button will work.
+    const memeContainer = document.createElement('div')
+    // adding class so that there is a way to select each new div created. 
+    memeContainer.classList.add('meme-container')
+
+    // this is appending all of the inputs to the new div called memeContainer
+    memeContainer.append(newMemeTopText, newMemeBottomText, newMemeImg, removeButton)
+
+    // this is appending the memeContainer element/div to the premade div with the id of results 
+    // this is so that we can add a single element to the div of results
+    document.querySelector('#results').appendChild(memeContainer)
 
     topText.value = ''
     bottomText.value = ''
@@ -95,45 +110,13 @@ function makeMeme() {
 
 }
 
-const removeMeme = document.getElementsByClassName('removeMe-button')
-for(let btn of removeMeme){
-    btn.addEventListener('click', function(e){
-        console.log('remove button')
-        console.log(e)
-        e.target.getElementClass().remove()
-    })
-
-}
-
-
+// making var to select the form with the intention of when the user clicks the Add Meme button 
+// it will then create the meme from the user's input
 const form = document.querySelector("#meme-Generator")
+// adding an event listener for the form so that you can make the add meme button preform an action.
 form.addEventListener('submit', function(evt) {
+    // this stops the form from being submitted to a server. 
     evt.preventDefault()
+    // this will exectue the function makeMeme() when Add Meme button is clicked.
     makeMeme()
 })
-
-// const genMeme = document.querySelector('#submit')
-// genMeme.addEventListener("click", function() {
-//     makeMeme()
-// })
-
-// const form = document.querySelector('.meme-Generator')
-// const newImg = document.querySelector('#memeImg')
-// const topText = document.querySelector('#topText')
-// const bottomText = document.querySelector('#bottomText')
-
-// const newMeme = document.querySelector('.newMeme')
-
-// form.addEventListener('submit', function(e) {
-//     e.preventDefault()
-//     const makeNewMeme = makeMeme(
-//         newImg.value,
-//         topText.value,
-//         bottomText.value
-//     )
-// })
-
-// function makeMeme(newImg, topText, bottomText) {
-//     const meme = document.createElement('div')
-    
-// }
